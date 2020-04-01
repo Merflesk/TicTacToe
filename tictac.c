adbeca1d2741233c5e3d918aa63faea39cda3fd1
Merflesk <joachim.mathisen@gmail.com>
2020-04-01 10:37:58 +0200

    Created project files

---
+++ tictac.c
@@ -0,0 +1,22 @@
+/* Program: TicTacToe aka 3 in a row
+ * Author: Joachim Larsen Mathisen
+ * 
+ * Rules of the game:
+ * Game board is a 3x3 grid with 9 cells.
+ * Each cell can be X or O or blank
+ * Each move will redraw the grid with a newly placed piece
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    /*
+    Entry point of the program.
+    TODO
+    While loop that keeps the program running until one player wins. 
+     */
+    return 0;
+}
+
+ 
